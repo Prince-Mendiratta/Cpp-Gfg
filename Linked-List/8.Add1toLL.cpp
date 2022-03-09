b6bf79a0a376e57cdc91da65df9885273659026c
Prince Mendiratta <prince.mendi@gmail.com>
2022-03-09 11:52:53 +0530

    Linked List [6/36]
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

---
+++ Linked-List/8.Add1toLL.cpp
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* addOne(Node *head) 
+    {
+        // Your Code here
+        // return head of list after adding one
+        struct Node *last = head->next, *temp = head;
+        while(last->next != NULL){
+            if(last->data < 9 && last->next->data == 9){
+                temp = last;
+            }
+            last = last->next;
+        }
+        if(last->data != 9){
+            last->data++;
+        }else{
+            if(temp->data == 9){
+                temp = new Node(0);
+                temp->next = head;
+                head = temp;
+            }
+            temp->data = temp->data++;
+            temp = temp->next;
+            while(temp != NULL){
+                temp->data = 0;
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
